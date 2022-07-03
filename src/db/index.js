7f866de5d5e8f6956043151546f12d2066aecbe7
vannakvy <vannakvy2020@gmail.com>
2022-07-04 06:57:20 +0700

    CHH

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,8 @@ const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'pern_auth',
-  password: 'root',
+  database: 'pos_db',
+  password: '1010',
   port: 5432,
 })
 
